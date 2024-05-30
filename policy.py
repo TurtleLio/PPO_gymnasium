23013d8065ddf5f802e01f40a59f6527149d6b49
Raz Beck <razbe123@gmail.com>
2024-05-30 18:24:53 +0300

    getting NaN errors

--- policy.py
+++ policy.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.distributions import Normal
+import numpy as np
 
 
 class PPOPolicy(nn.Module):
@@ -129,3 +130,94 @@ class PPOPolicy(nn.Module):
             torch.exp(self.log_std).detach(),
             stop
         )
+
+def compute_return_advantage(rewards, values, mask, gamma, gae_lambda, last_value):
+    """
+    Computes returns and advantage based on generalized advantage estimation.
+    """
+    #N = rewards.shape[0]
+    N = len(rewards)
+    advantages = np.zeros(
+        (N, 1),
+        dtype=np.float32
+    )
+
+    tmp = 0.0
+    for k in reversed(range(N)):
+        if k == (N-1):
+            next_values = last_value
+        # if k == N - 1:
+        #     next_non_terminal = 1 - is_last_terminal
+        #     next_values = last_value
+        else:
+            #next_non_terminal = 1
+            next_values = values[k+1]
+        next_non_terminal = mask[k]
+        delta = (rewards[k] +
+                 gamma * next_non_terminal * next_values -
+                 values[k])
+
+        tmp = delta + gamma * gae_lambda * next_non_terminal * tmp
+
+        advantages[k] = tmp
+    advantages = [item for sublist in advantages for item in sublist]
+    values = np.array(values)
+    returns = advantages + values
+
+    return returns, advantages
+
+
+def get_mini_batch(train_data, batch_size):
+    rng = np.random.default_rng()
+    # Check that batch_size is smaller than the number of data points
+    assert batch_size <= len(train_data[0]), \
+        "Batch size must be smaller than number of data."
+
+    # Get the number of data points
+    data_size = len(train_data[0])
+
+    # Create and shuffle indices
+    indices = np.arange(data_size)
+    rng.shuffle(indices)
+
+    # Split indices into mini-batches
+    split_indices = []
+    point = batch_size
+    while point < data_size:
+        split_indices.append(point)
+        point += batch_size
+
+    # Split data based on shuffled indices
+    temp_data = {
+        'prev_obs_vec': np.split(train_data[0][indices], split_indices),
+        'obs_vec': np.split(train_data[1][indices], split_indices),
+        'action_vec': np.split(train_data[2][indices], split_indices),
+        'log_vec': np.split(train_data[3][indices], split_indices),
+        'advantage': np.split(train_data[4][indices], split_indices),
+        'reward_vec': np.split(train_data[5][indices], split_indices),
+        'value_vec': np.split(train_data[6][indices], split_indices),
+        'returns': np.split(train_data[7][indices], split_indices)
+    }
+    # temp_data = [[] for _ in range(8)]
+    # for i in range(8):
+    #     # Gather the data for each index before splitting
+    #     temp_data[i] = np.array([train_data[i][index] for index in indices])
+    # temp_data = [np.split(temp_data[i][indices], split_indices) for i in range(8)]
+    # Prepare mini-batches as a list of dictionaries
+    n = len(temp_data['prev_obs_vec'])
+    data_out = []
+    for k in range(n):
+        data_out.append(
+            {
+                'prev_obs_vec': temp_data['prev_obs_vec'][k],
+                'obs_vec': temp_data['obs_vec'][k],
+                'action_vec': temp_data['action_vec'][k],
+                'log_vec': temp_data['log_vec'][k],
+                'advantage': temp_data['advantage'][k],
+                'reward_vec': temp_data['reward_vec'][k],
+                'value_vec': temp_data['value_vec'][k],
+                'returns': temp_data['returns'][k],
+            }
+        )
+
+    return data_out
--- train.py
+++ train.py
@@ -11,8 +11,9 @@ import gymnasium as gym
 
 import matplotlib.pyplot as plt
 
-from policy import PPOPolicy
-from ppobuffer import PPOBuffer
+import policy
+#from policy import PPOPolicy
+
 
 #from test_pendulum import play_new_game
 
@@ -66,6 +67,14 @@ class V_Network(nn.Module):
 
         return values
 
+def organizing_array(array):
+    # Flatten the nested list and ndarray structure
+    single_value_ndarrays = []
+    for sublist in array:
+        for item in sublist:
+            single_value_ndarrays.append(np.array([item]))
+
+    return single_value_ndarrays
 
 if __name__ == "__main__":
 
@@ -80,9 +89,9 @@ if __name__ == "__main__":
 
     learning_rate = 3e-4
 
-    buffer = PPOBuffer(obs_dim, action_dim, NUM_STEPS)
+    #buffer = PPOBuffer(obs_dim, action_dim, NUM_STEPS)
 
-    policy = PPOPolicy(
+    trainer = policy.PPOPolicy(
         pi_network,
         v_network,
         learning_rate,
@@ -102,13 +111,17 @@ if __name__ == "__main__":
     mean_rewards = []
 
     obs, _ = env.reset()
-
+    reward_vec, value_vec, log_vec, advantage_vec, prev_obs_vec, obs_vec, action_vec, returns_vec, masks = [], [], [], [], [], [], [], [], []
+    train_data = [[] for _ in range(8)]  # prev_obs_vec, obs_vec, action_vec, log_vec, advantage_mean_vec, reward_vec, value_vec, return_vec
+    last_value_vec = 0
+    best_episode_reward = 0
+    best_episode_reward_vec = []
     for t in range(TOTAL_TIMESTEPS):
 
         if t % REPORT_STEPS == 0:
             print(t, '/', TOTAL_TIMESTEPS)
 
-        action, log_prob, values = policy.get_action(obs)
+        action, log_prob, values = trainer.get_action(obs)
 
         clipped_action = np.clip(action, lower_bound, upper_bound)
 
@@ -117,9 +130,17 @@ if __name__ == "__main__":
         done = terminated or truncated
 
         ep_reward += reward
-
+        values = values.item()
+        log_prob = log_prob.item()
+        masks.append(1 - done)
+        prev_obs_vec.append(obs)
+        obs_vec.append(next_obs)
+        action_vec.append(action)
+        reward_vec.append(reward)
+        log_vec.append(log_prob)
+        value_vec.append(values)
         # Add to buffer
-        buffer.record(obs, action, reward, values, log_prob)
+        #buffer.record(obs, action, reward, values, log_prob)
 
         obs = next_obs
 
@@ -129,21 +150,46 @@ if __name__ == "__main__":
             if done:
                 ep_count += 1
             # Value of last time-step
-            last_value = policy.get_values(obs)
+            if best_episode_reward < ep_reward or best_episode_reward == 0:
+                best_episode_reward = ep_reward
+            ep_reward = 0
+            last_value = trainer.get_values(obs)
 
             # Compute returns and advantage and store in buffer
-            buffer.process_trajectory(
-                gamma=GAMMA,
-                gae_lam=GAE_LAM,
-                is_last_terminal=done,
-                last_v=last_value)
+            # buffer.process_trajectory(
+            #     gamma=GAMMA,
+            #     gae_lam=GAE_LAM,
+            #     is_last_terminal=done,
+            #     last_v=last_value)
+            returns, advantage = policy.compute_return_advantage(reward_vec, value_vec, masks, GAMMA, GAE_LAM,
+                                                             last_value)
+            returns_vec.append(returns)
+            advantage_vec.append(advantage)
+            ep_reward = 0
             obs, _ = env.reset()
 
         if (t + 1) % NUM_STEPS == 0:
+
+            advantage_vec = organizing_array(advantage_vec)
+            returns_vec = organizing_array(returns_vec)
+            train_data[0].append(prev_obs_vec)
+            train_data[1].append(obs_vec)
+            train_data[2].append(action_vec)
+            train_data[3].append(log_vec)
+            train_data[4].append(advantage_vec)
+            train_data[5].append(reward_vec)
+            train_data[6].append(value_vec)
+            train_data[7].append(returns_vec)
+            print("Batch complete")
+            print(f"best episode reward = {best_episode_reward}")
+            best_episode_reward_vec.append(best_episode_reward)
+            best_episode_reward = 0
             season_count += 1
+            for i in range(8):
+                train_data[i] = np.concatenate(train_data[i])
             # Update for epochs
             for ep in range(NUM_EPOCHS):
-                batch_data = buffer.get_mini_batch(BATCH_SIZE)
+                batch_data = policy.get_mini_batch(train_data, BATCH_SIZE)
                 num_grads = len(batch_data)
 
                 # Iterate over minibatch of data
@@ -155,11 +201,11 @@ if __name__ == "__main__":
                         advantage_batch,
                         return_batch,
                     ) = (
-                        batch_data[k]['obs'],
-                        batch_data[k]['action'],
-                        batch_data[k]['log_prob'],
+                        batch_data[k]['prev_obs_vec'],
+                        batch_data[k]['action_vec'],
+                        batch_data[k]['log_vec'],
                         batch_data[k]['advantage'],
-                        batch_data[k]['return'],
+                        batch_data[k]['returns'],
                     )
 
                     # Normalize advantage
@@ -190,25 +236,28 @@ if __name__ == "__main__":
                         total_loss,
                         approx_kl,
                         std, stop
-                    ) = policy.update(obs_batch, action_batch,
+                    ) = trainer.update(obs_batch, action_batch,
                                       log_prob_batch, advantage_batch,
                                       return_batch)
-
-                    pi_losses.append(pi_loss.numpy())
-                    v_losses.append(v_loss.numpy())
-                    total_losses.append(total_loss.numpy())
-                    approx_kls.append(approx_kl.numpy())
-                    stds.append(std.numpy())
                     if ep == 0 and k == 0:
+                        pi_losses.append(pi_loss.numpy())
+                        v_losses.append(v_loss.numpy())
+                        total_losses.append(total_loss.numpy())
+                        approx_kls.append(approx_kl.numpy())
+                        stds.append(std.numpy())
                         print(f"start of training: pi loss: {pi_loss} | v loss: {v_loss}| total loss: {total_loss}")
                 if stop:
                     print(f"target kl achived after {ep} iterations")
                     break
             print(f"end of training: pi loss: {pi_loss} | v loss: {v_loss}| total loss: {total_loss}")
-            buffer.clear()
-
+            #buffer.clear()
+            reward_vec, value_vec, log_vec, advantage_vec, prev_obs_vec, obs_vec, action_vec, returns_vec, masks = [], [], [], [], [], [], [], [], []
+            train_data = [[] for _ in range(8)]  # prev_obs_vec, obs_vec, action_vec, log_vec, advantage_mean_vec, reward_vec, value_vec, return_vec
             mean_ep_reward = ep_reward / ep_count
             ep_reward, ep_count = 0.0, 0
+            last_value_vec = 0
+            best_ep_reward = 0
+
 
             mean_rewards.append(mean_ep_reward)
             pi_losses, v_losses, total_losses, approx_kls, stds = (
@@ -225,6 +274,34 @@ if __name__ == "__main__":
     ax.set_xlabel("season")
     ax.set_ylabel("episodic reward")
     ax.grid(True)
+    plt.show()
+
+    plt.figure()
+    plt.plot(range(len(best_episode_reward_vec)), best_episode_reward_vec)
+    plt.title('best episode reward')
+    plt.xlabel('epoch')
+    plt.ylabel('reward')
+    plt.show()
+
+    plt.figure()
+    plt.plot(range(len(pi_losses)), pi_losses)
+    plt.title('pi_losses')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.show()
+
+    plt.figure()
+    plt.plot(range(len(v_losses)), v_losses)
+    plt.title('v_losses')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.show()
+
+    plt.figure()
+    plt.plot(range(len(total_losses)), total_losses)
+    plt.title('total_losses')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
     plt.show()
 
     #play_new_game(policy)
